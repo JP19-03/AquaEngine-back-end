7812373f4cd99404d93d3910435f00e504578e26
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-22 18:54:50 -0500

    feat(control): add inbound service contract.

---
+++ AquaEngine.API/Control/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using AquaEngine.API.Control.Domain.Model.Aggregates;
+using AquaEngine.API.Shared.Domain.Repositories;
+
+namespace AquaEngine.API.Control.Domain.Repositories;
+
+public interface IProductRepository : IBaseRepository<Product>
+{
+    Task<Product?> FindByNameAsync(string name);
+    Task<IEnumerable<Product>> FindByUserIdAsync(long UserId);
+}
