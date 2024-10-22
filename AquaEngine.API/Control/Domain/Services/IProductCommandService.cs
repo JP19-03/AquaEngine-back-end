9e9956c5f6d3c4f462e3cda318fc8906b2c0b089
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-22 18:34:27 -0500

    feat(control): add command and query service contracts.

---
+++ AquaEngine.API/Control/Domain/Services/IProductCommandService.cs
@@ -0,0 +1,12 @@
+﻿using AquaEngine.API.Control.Domain.Model.Aggregates;
+using AquaEngine.API.Control.Domain.Model.Commands;
+
+namespace AquaEngine.API.Control.Domain.Services;
+
+public interface IProductCommandService
+{
+    Task<Product?> handle(CreateProductCommand command);
+    Task<Product?> handle(DecreaseQuantityCommand command);
+    Task<Product?> handle(IncreaseQuantityCommand command);
+    Task<Product?> handle(UpdateProductOwnerCommand command);
+}
---
+++ AquaEngine.API/Control/Domain/Services/IProductQueryService.cs
@@ -0,0 +1,11 @@
+﻿using AquaEngine.API.Control.Domain.Model.Aggregates;
+using AquaEngine.API.Control.Domain.Model.Queries;
+
+namespace AquaEngine.API.Control.Domain.Services;
+
+public interface IProductQueryService
+{
+    Task<List<Product>> handle(GetProducctByUserIdQuery query);
+    Task<Product?> handle(GetProductByIdQuery query);
+    Task<Product?> handle(GetProductByNameQuery query);
+}
