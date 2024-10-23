58476d78b054419f8466f1c9d12acb9aade630f7
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-23 11:16:20 -0500

    feat(control): add interface layer assemblers.

---
+++ AquaEngine.API/Control/Interfaces/REST/Transform/IncreaseQuantityCommandFromResourceAssembler.cs
@@ -0,0 +1,11 @@
+﻿using AquaEngine.API.Control.Interfaces.REST.Resources;
+
+namespace AquaEngine.API.Control.Interfaces.REST.Transform;
+
+public class IncreaseQuantityCommandFromResourceAssembler
+{
+    public static IncreaseQuantityResource ToCommandFromResource(IncreaseQuantityResource resource)
+    {
+        return new IncreaseQuantityResource(resource.Quantity, (int)resource.ProductId);
+    }
+}
---
+++ AquaEngine.API/Control/Interfaces/REST/Transform/ProductResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using AquaEngine.API.Control.Domain.Model.Aggregates;
+using AquaEngine.API.Control.Interfaces.REST.Resources;
+
+namespace AquaEngine.API.Control.Interfaces.REST.Transform;
+
+public class ProductResourceFromEntityAssembler
+{
+    public static ProductResource ToResourceFromEntity(Product product)
+    {
+        return new ProductResource(product.Id, product.UserId.userId, product.Name, product.QuantityPerUnit, product.UnitPrice, product.Quantity);
+    }
+}
