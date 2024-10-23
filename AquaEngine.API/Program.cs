ea38c38b526f8804f76fbbe7bf87af92a5ca4039
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-23 11:51:39 -0500

    feat(control): add program configuration.

--- AquaEngine.API/Program.cs
+++ AquaEngine.API/Program.cs
@@ -1,20 +1,27 @@
 
 
+using AquaEngine.API.Control.Application.Internal.CommandServices;
+using AquaEngine.API.Control.Application.Internal.QueryServices;
+using AquaEngine.API.Control.Domain.Repositories;
+using AquaEngine.API.Control.Domain.Services;
+using AquaEngine.API.Control.Infrastructure.Persistence.EFC.Repositories;
+using AquaEngine.API.Shared.Domain.Repositories;
 using AquaEngine.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using AquaEngine.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using AquaEngine.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRouting(options=>options.LowercaseUrls = true);
 builder.Services.AddControllers(options=> options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options=> options.EnableAnnotations());
 
-
 // Add Database Connection String
-/* Modificar dependiendo del feature, en el merge solo hacemos la conexion en appdb context y ya esto le quitamos la anotacion
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 if (connectionString is null)
@@ -29,26 +36,22 @@ if (builder.Environment.IsDevelopment())
             .EnableDetailedErrors();
     });
 else if (builder.Environment.IsProduction())
+{
     builder.Services.AddDbContext<AppDbContext>(options =>
     {
         options.UseMySQL(connectionString)
             .LogTo(Console.WriteLine, LogLevel.Information)
             .EnableDetailedErrors();
     });
-
- */
+}
 
 // Configure Dependency Injection
+builder.Services.AddScoped<IUnitOfWOrk, UnitOfWork>();
 
-
-// Analytics Bounded Context Dependency Injection
-
-// Sales Bounded Context Dependency Injection
-
-// Control Bounded Context Dependency Injection
-
-// Etc...
-
+// News Bounded Context Dependency Injection
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductCommandService, ProductCommandService>();
+builder.Services.AddScoped<IProductQueryService, ProductQueryService>();
 
 var app = builder.Build();
 
