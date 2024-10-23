91619c85a69f272ca002322328cd446f3ce054fe
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-22 19:52:41 -0500

    feat(control): add query service implementation.

---
+++ AquaEngine.API/Control/Application/Internal/QueryServices/ProductQueryService.cs
@@ -0,0 +1,24 @@
+﻿using AquaEngine.API.Control.Domain.Model.Aggregates;
+using AquaEngine.API.Control.Domain.Model.Queries;
+using AquaEngine.API.Control.Domain.Repositories;
+using AquaEngine.API.Control.Domain.Services;
+
+namespace AquaEngine.API.Control.Application.Internal.QueryServices;
+
+public class ProductQueryService(IProductRepository productRepository) : IProductQueryService
+{
+    public Task<IEnumerable<Product>> handle(GetProducctByUserIdQuery query)
+    {
+        return productRepository.FindByUserIdAsync(query.UserId.userId);
+    }
+
+    public async Task<Product?> handle(GetProductByIdQuery query)
+    {
+        return await productRepository.FindByIdAsync((int)query.ProductId);
+    }
+
+    public async Task<Product?> handle(GetProductByNameQuery query)
+    {
+        return await productRepository.FindByNameAsync(query.Name);
+    }
+}
