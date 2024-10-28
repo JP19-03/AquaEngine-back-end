46f857400d2aaae4a35f8ec17cba06008a8bd5a2
teocchiii <u202211212@upc.edu.pe>
2024-10-28 00:48:51 -0500

    feat(maintenance): implemented the interfaces used in the domain layer to execute commands and queries.

---
+++ AquaEngine.API/Analytics/Application/Internal/CommandServices/MaintenanceCommandService.cs
@@ -0,0 +1,31 @@
+using AquaEngine.API.Analytics.Domain.Model.Aggregate;
+using AquaEngine.API.Analytics.Domain.Model.Commands;
+using AquaEngine.API.Analytics.Domain.Repositories;
+using AquaEngine.API.Analytics.Domain.Services;
+using AquaEngine.API.Shared.Domain.Repositories;
+
+namespace AquaEngine.API.Analytics.Application.Internal.CommandServices;
+
+public class MaintenanceCommandService(IMaintenanceRepository maintenanceRepository,IUnitOfWOrk unitOfWOrk)
+:IMaintenanceCommandService
+{
+    public  async Task<Maintenance?> Handle(CreateMaintenanceCommand command)
+    {
+        //Considere hacer validaciones pero maitenance puede registrar dos mantenimientos hechos por un solo tecnico o dos mantenimientos por el mismo error, en este caso no necesita
+        
+        var maintenance = new Maintenance(command);
+
+        try
+        {
+            await maintenanceRepository.AddAsync(maintenance);
+            await unitOfWOrk.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return maintenance;
+    }
+}
