36b83d02b17a6d74cd551172100c28dfda1d817d
teocchiii <u202211212@upc.edu.pe>
2024-10-28 00:47:35 -0500

    feat(maintenance): added interfaces to implement later on into the application layer.

---
+++ AquaEngine.API/Analytics/Domain/Services/IMaintenanceQueryService.cs
@@ -0,0 +1,11 @@
+using AquaEngine.API.Analytics.Domain.Model.Aggregate;
+using AquaEngine.API.Analytics.Domain.Model.Queries;
+
+namespace AquaEngine.API.Analytics.Domain.Services;
+
+public interface IMaintenanceQueryService
+{
+    Task<IEnumerable<Maintenance>> Handle(GetMaintenanceByMachineIdQuery query);
+    
+    Task<Maintenance?> Handle(GetMaintenanceByIssueTypeQuery query);
+}
