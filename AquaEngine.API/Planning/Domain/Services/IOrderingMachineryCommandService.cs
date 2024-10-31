624bd1046218c7be0dbd0cddcdcacd929496915c
Pedro Guia <u202212010@upc.edu.pe>
2024-10-31 13:01:41 -0500

    feat(planning): finished the domain layer.

---
+++ AquaEngine.API/Planning/Domain/Services/IOrderingMachineryCommandService.cs
@@ -0,0 +1,10 @@
+using AquaEngine.API.Planning.Domain.Model.Aggregates;
+using AquaEngine.API.Planning.Domain.Model.Commands;
+
+namespace AquaEngine.API.Planning.Domain.Services;
+
+public interface IOrderingMachineryCommandService
+{
+    Task<OrderingMachinery> Handle(CreateOrderingMachineryCommand command);
+    Task<OrderingMachinery> Handle(UpdateOrderingStatusCommand command);
+}
---
+++ AquaEngine.API/Planning/Domain/Services/IOrderingMachineryQueryService.cs
@@ -0,0 +1,13 @@
+using AquaEngine.API.Planning.Domain.Model.Aggregates;
+using AquaEngine.API.Planning.Domain.Queries;
+
+namespace AquaEngine.API.Planning.Domain.Services;
+
+public interface IOrderingMachineryQueryService
+{
+    Task<IEnumerable<OrderingMachinery>> Handle(GetOrderingMachineryByUserIdQuery query);
+
+    Task<OrderingMachinery?> Handle(GetOrderingMachineryByStatus query);
+
+    Task<OrderingMachinery?> Handle(GetOrderingMachineryByIdQuery query);
+}
