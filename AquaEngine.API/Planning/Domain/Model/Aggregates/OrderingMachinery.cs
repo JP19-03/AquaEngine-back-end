af4ff168530553f616209b4e956fc0e0c6eaddb7
Pedro Guia <u202212010@upc.edu.pe>
2024-10-31 12:25:37 -0500

    feat(planning): added missing attribute IssueType into the constructor of the aggregate.

---
+++ AquaEngine.API/Planning/Domain/Model/Aggregates/OrderingMachinery.cs
@@ -0,0 +1,8 @@
+namespace AquaEngine.API.Planning.Domain.Model.Aggregates;
+
+public class OrderingMachinery
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string urlToImage { get; private set; }
+}
