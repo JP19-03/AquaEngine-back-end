d10fb737451db38adb88c0d1065342e29e622f85
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-11 00:20:20 -0500

    feat(iam): add command and query service implementations.

---
+++ AquaEngine.API/IAM/Application/Internal/CommandServices/UserQueryService.cs
@@ -0,0 +1,21 @@
+﻿using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Aggregates;
+using AquaEngine.API.IAM.Domain.Model.Queries;
+using AquaEngine.API.IAM.Domain.Repositories;
+using AquaEngine.API.IAM.Domain.Services;
+
+namespace AquaEngine.API.IAM.Application.Internal.CommandServices;
+
+/// <summary>
+/// User query service. 
+/// </summary>
+/// <param name="userRepository">
+/// The <see cref="IUserRepository"/> instance.
+/// </param>
+public class UserQueryService(IUserRepository userRepository) : IUserQueryService
+{
+    // <inheritdoc/>
+    public async Task<User?> Handle(GetUserByIdQuery query)
+    {
+        return await userRepository.FindByIdAsync(query.Id);
+    }
+}
