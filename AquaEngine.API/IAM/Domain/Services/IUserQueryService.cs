a8f609a91db7f469e11ae4719411e00fd52534d8
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-11-11 00:10:31 -0500

    feat(iam): add user command and query service contracts.

---
+++ AquaEngine.API/IAM/Domain/Services/IUserQueryService.cs
@@ -0,0 +1,24 @@
+﻿using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Aggregates;
+using AquaEngine.API.IAM.Domain.Model.Queries;
+
+namespace AquaEngine.API.IAM.Domain.Services;
+
+/// <summary>
+/// User query service 
+/// </summary>
+/// <remarks>
+/// This service is responsible for handling user queries
+/// </remarks>
+public interface IUserQueryService
+{
+    /// <summary>
+    /// Handle get user by id query 
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetUserByIdQuery"/> query containing user id
+    /// </param>
+    /// <returns>
+    /// A task containing the <see cref="User"/> user if found, otherwise null
+    /// </returns>
+    Task<User?> Handle(GetUserByIdQuery query);
+}
