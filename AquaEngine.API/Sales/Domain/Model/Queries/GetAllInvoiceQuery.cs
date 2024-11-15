5430359273506bdcc46be6bbafa868b2bd16801a
Mitawarmi2000 <u202214130@upc.edu.pe>
2024-11-15 00:47:45 -0500

    refactor: the router invoice for sales.

--- AquaEngine.API/Sales/Domain/Model/Queries/GetAllInvoiceQuery.cs
+++ AquaEngine.API/Sales/Domain/Model/Queries/GetAllInvoiceQuery.cs
@@ -1,4 +1,4 @@
-namespace AquaEngine.API.Invoice.Domain.Model.Queries;
+namespace AquaEngine.API.Sales.Domain.Model.Queries;
 /// <summary>
 /// Represent a query to get all ordering machinery in the Aqua Engine Platform.
 /// </summary>
