e13175cf564cecc78f085d4db10f50eda772d464
Pedro Guia <u202212010@upc.edu.pe>
2024-11-15 14:07:14 -0500

    feat(Planning): update ordering machinery command.

--- AquaEngine.API/Planning/Domain/Model/Aggregates/OrderingMachinery.cs
+++ AquaEngine.API/Planning/Domain/Model/Aggregates/OrderingMachinery.cs
@@ -1,4 +1,5 @@
 using AquaEngine.API.Planning.Domain.Model.Commands;
+using AquaEngine.API.Planning.Domain.Model.ValueObjects;
 
 namespace AquaEngine.API.Planning.Domain.Model.Aggregates;
 
@@ -9,18 +10,23 @@ public class OrderingMachinery
     public string UrlToImage { get; private set; }
     public string Status { get; private set; }
     
-    public OrderingMachinery(string name, string urlToImage, string status)
+    public StockAspect StockAspect { get; private set; }
+    
+    public OrderingMachinery(string name, string urlToImage, string status, StockAspect stockAspect)
     {
         Name = name;
         UrlToImage = urlToImage;
         Status = status;
+        StockAspect = stockAspect;
     }
 
     public OrderingMachinery(CreateOrderingMachineryCommand command)
     {
+        Id = command.Id;
         Name = command.Name;
         UrlToImage = command.UrlToImage;
         Status = command.Status;
+        StockAspect = command.StockAspect;
     }
     public void UpdateStatus(UpdateOrderingStatusCommand command)
     {
